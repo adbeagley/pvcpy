29abb28392765c3433f9b0a56dd09d888d97d656
adbeagley <91222651+adbeagley@users.noreply.github.com>
2023-05-15 16:00:55 -0700

    testing

--- run_tests.py
+++ run_tests.py
@@ -3,8 +3,8 @@ import pytest
 
 if __name__ == "__main__":
     import pvcpy
-    src_dcm = r"E:\1_Masters\!Data\GSA\GSA_11\GSA_11_dicom_scap_raw"
-    mask_dcm = r"E:\1_Masters\!Data\GSA\GSA_11\GSA_11_dicom_scap_mask"
+    src_dcm = r"Z:\Aren\Porcine PVC\Specimen 3\DICOMS\S3_Raw"
+    mask_dcm = r"Z:\Aren\Porcine PVC\Specimen 3\DICOMS\S3_Mask"
     dcm = pvcpy.surface_pvc(src_dcm_dir=src_dcm, mask_dcm_dir=mask_dcm)
     print(dcm)
 
